9df42ed86643bef0c1f95d97759e65755c43e73a
Ronak1414 <96277743+Ronak1414@users.noreply.github.com>
2022-05-15 16:06:10 +0530

    Add files via upload

---
+++ area + file .py
@@ -0,0 +1,11 @@
+ra=input("radius")
+r=float(ra)
+area=3.14*r*r
+print("area = ",area)
+
+
+
+
+file=str(input("Enter the file name :"))
+file_extension=file.split(".")
+print("The file extension is :" ,file_extension[-1])
